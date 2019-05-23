fee613f4edd81f03c493a7c223be44a082d22e50
Olivier St-Louis-Desjardins <Olivier.St-Louis-Desjardins@radio-canada.ca>
2019-05-23 08:16:49 -0400

    woops added file

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+
+function mapE6NGlobalResultsToStructure(e6nGlobalResults: object): object | null;
+function mapE6NStaticResultsToStructure(e6nStaticResults: object): object | null;
+
+export const result = {
+    mapE6NGlobalResultsToStructure,
+    mapE6NStaticResultsToStructure,
+};
