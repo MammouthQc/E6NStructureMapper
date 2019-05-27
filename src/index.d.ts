1cfb450b61e417de573ba1177052fe74aa52dafd
Olivier St-Louis-Desjardins <Olivier.St-Louis-Desjardins@radio-canada.ca>
2019-05-27 10:55:22 -0400

    Update typescript, explicit typing

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,93 @@
-type GlobalStructureInputType = object;
-type GlobalStructureResultType = object;
-type StaticStructureInputType = object;
-type StaticStructureResultType = object;
+export type GlobalStructureInputType = object; // Changes too often to be typed
+export type StaticStructureInputType = object; // Changes too often to be typed
+
+export type GlobalStructureResultType = {
+    alerts: GlobalAlert[],
+    candidates: GlobalCandidate[],
+    closedPolls: number,
+    electedNumber: number,
+    governmentCall: GlobalGovernmentCall,
+    lists: GlobalList[],
+    noResultsRidingsNumber: number,
+    pollingTime: number,
+    parties: GlobalParty[],
+    previousTimestamp: string,
+    refreshToken: string,
+    ridings: GlobalRiding[],
+    ridingsNumberWithoutElected: number,
+    totalPolls: number,
+    voteNumber: number,
+};
+
+type GlobalAlert = {
+    candidateId: number,
+    date: string,
+    isVIP: boolean,
+    ridingId: number,
+};
+
+type GlobalCandidate = {
+    differentialTextual: string,
+    id: number,
+    status: string,
+    voteNumber: number,
+    voteNumberTextual: string,
+    votePercent: number,
+    votePercentTextual: string,
+};
+
+type GlobalList = {
+    id: number,
+    referencedIds: number[],
+};
+
+type GlobalParty = {
+    aheadNumber: number,
+    electedNumber: number,
+    id: number,
+    status: string,
+    voteNumber: number,
+    voteNumberTextual: string,
+    votePercent: number,
+    votePercentTextual: string,
+};
+
+type GlobalRiding = {  
+    closedPolls: number,
+    differentialTextual: string,
+    id: number,
+    leadingPartyId: number,
+    pollsTotal: number,
+    status: string,
+    voteNumber: number,
+    voteNumberTextual: string,
+};
+
+type GlobalGovernmentCall = {
+    partyId: number,
+    status: number,
+};
+
+export type StaticStructureResultType = {
+    candidates: object, // Properties are mapped by the identifiers of the candidates
+    lists: StaticList[],
+    name: string,
+    parties: object,  // Properties are mapped by the identifiers of the parties
+    ridings: object,  // Properties are mapped by the identifiers of the ridings
+};
+
+type StaticList = {
+    name: string,
+    lists: StaticListData[]
+};
+
+type StaticListData = {
+    id: number,
+    name: string,
+    referencedIds: number[],
+    type: number,
+};
+
 
 export function mapE6NGlobalResultsToStructure(e6nGlobalResults: GlobalStructureInputType): GlobalStructureResultType;
 
